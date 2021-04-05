793cb876241b8b1eaf63b8101169335f3eaa6fdd
William Chen <z.william.chen@gmail.com>
2021-04-04 23:21:58 -0700

    moved file to show test

--- src/test/java/com/wilchen/spellchecker/SpellCheckerTest.java
+++ src/test/java/com/wilchen/spellchecker/SpellCheckerTest.java
@@ -1,13 +1,9 @@
+package com.wilchen.spellchecker;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import com.wilchen.spellchecker.SpellChecker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-
-import java.util.HashSet;
 
 public class SpellCheckerTest {
 
